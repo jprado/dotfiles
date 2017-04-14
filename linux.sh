03bd3746823d0421089dd6447a0fb45890601a83
jaime <jaimeprado@gmail.com>
2017-04-14 15:22:11 -0700

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,9 +1,14 @@
-#vim
-#virtualbox
-#ansible/paramiko
-#subversion
-#python-pip
-#vagrant
-#chrome
-#jq
-#apt-get install default-jre
+#!/usr/bin/env bash
+
+sudo apt-get install subversion
+sudo apt-get install ansible
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo apt-get install libfontconfig
+sudo npm install gulp -g
+
+npm install penthouse
+npm install gulp-minify-css
+npm install gulp-concat
+npm install gulp-autoprefixer
+npm install gulp-cssbeautify
