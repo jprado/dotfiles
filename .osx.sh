b6c115caa56c5a4c96c1201aeb9270625d430d5e
jaime <jaimeprado@gmail.com>
2018-01-22 09:10:21 -0800

    Update .osx.sh

--- .osx.sh
+++ .osx.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 # ~/.osx
 
-# Disable the “Are you sure you want to open this application?” dialog
-defaults write com.apple.LaunchServices LSQuarantine -bool false
-
-# Disable the sudden motion sensor as it’s not useful for SSDs
-sudo pmset -a sms 0
-
 # Add iOS Simulator to Launchpad
 sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/iOS Simulator.app" "/Applications/iOS Simulator.app"
