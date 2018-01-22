ed717118d14a0cd8b1229907fc835cafec4ae66e
jaime <jaimeprado@gmail.com>
2018-01-22 09:08:22 -0800

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
 sudo apt-get install subversion
-sudo apt-get install git-svn
-sudo apt-get install ansible
-sudo apt-get install nodejs
-sudo apt-get install libfontconfig
 sudo apt-get install php7.0-cli
 sudo npm install gulp -g
