8615e2373d85baf509f70597dacd2fa9935732ef
jaime <jaimeprado@gmail.com>
2018-01-22 09:07:33 -0800

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -4,8 +4,8 @@ sudo apt-get install subversion
 sudo apt-get install git-svn
 sudo apt-get install ansible
 sudo apt-get install nodejs
-sudo apt-get install npm
 sudo apt-get install libfontconfig
+sudo apt-get install php7.0-cli
 sudo npm install gulp -g
 
 npm install penthouse
