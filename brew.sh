e827ecc186d604826e08b9b83f81f9e4f8014aed
jaime <jaimeprado@gmail.com>
2018-04-07 20:55:30 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -7,37 +7,17 @@ brew tap caskroom/versions
 
 brew update && brew upgrade && brew upgrade brew-cask && brew cleanup && brew cask cleanup
 
-brew install ansible
-brew install duti
-brew install htop
-brew install node
-brew install findutils
-brew install ffmpeg --with-libvpx
+brew install nvm
 brew install git
-brew install homebrew/dupes/grep
-brew install jq
-brew install subversion
-brew install git-svn
-brew install tmux
-brew install wget
-brew install vagrant
 brew install vim
+brew install zsh
 
-brew cask install beyond-compare
-brew cask install bittorrent-sync
-brew cask install filezilla
 brew cask install google-chrome
-brew cask install google-chrome-canary
-brew cask install iterm2
-brew cask install jing
-brew cask install java
+brew cask install itsycal
 brew cask install libreoffice
-brew cask install opera
-brew cask install skype
-brew cask install slack
+brew cask install resilio-sync
 brew cask install sublime-text
 brew cask install spectacle
-brew cask install virtualbox
-brew cask install vlc
+brew cask install visual-studio-code
 
 brew cleanup
