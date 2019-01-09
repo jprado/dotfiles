73a2339ccd74890119b92cb6eafd56fa92795364
jaime <jaimeprado@gmail.com>
2019-01-09 13:45:51 -0800

    Delete .osx.sh

--- .osx.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# ~/.osx
-
-# Add iOS Simulator to Launchpad
-sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/iOS Simulator.app" "/Applications/iOS Simulator.app"
