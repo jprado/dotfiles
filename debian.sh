01ff7e305ab614978e51d06a984d0cc09b7cde92
jaime <jaimeprado@gmail.com>
2019-03-19 20:00:41 -0700

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,3 +1,3 @@
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoclean && sudo apt-get autoremove
-sudo apt-get install vim curl zsh
+sudo apt-get install vim curl zsh git
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
