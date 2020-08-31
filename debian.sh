baa4d016eaebc19b3d1515afb79fc165820f1228
jaime <jaimeprado@gmail.com>
2020-08-31 12:08:29 -0700

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -3,8 +3,6 @@ sudo apt-get install vim curl zsh git subversion php7.0
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
-
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install --no-install-recommends yarn
