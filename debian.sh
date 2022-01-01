6cf051ef179cc3bfdffb88fb870bafbb2930b920
jaime <jaimeprado@gmail.com>
2022-01-01 14:23:01 -0800

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,5 +1,5 @@
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoclean && sudo apt-get autoremove
-sudo apt-get install vim curl zsh git subversion php7.0
+sudo apt-get install vim curl zsh git subversion php8.0 exa fzf
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
 
